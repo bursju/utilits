1df0aa4df7b0096dc442a75dcc7133edd76fa0fa
bursju <sergey@paykey.com>
2021-06-16 18:37:10 +0300

    add logic to view; offer screen

--- app/src/main/java/com/paykey/brsju/kbutils/horoscope/HoroscopeActivity.kt
+++ app/src/main/java/com/paykey/brsju/kbutils/horoscope/HoroscopeActivity.kt
@@ -16,76 +16,8 @@ import timber.log.Timber
 
 class HoroscopeActivity : AppCompatActivity() {
 
-    private lateinit var photoAdapter: PhotoAdapter
-    private var dataList = mutableListOf<ZodiacModel>()
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var bBack: ImageView
-    private lateinit var tvOffer: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_goroskop)
-
-        val root = findViewById<HoroscopeView>(R.id.root)
-
-        recyclerView = findViewById(R.id.rvZodiacsGrid)
-        bBack = findViewById(R.id.ivBack)
-        tvOffer = findViewById(R.id.tvOffer)
-
-        recyclerView.layoutManager = GridLayoutManager(applicationContext, 3)
-        photoAdapter = PhotoAdapter {
-
-            val goroskopApi = HoroscopeApi.client().create(HoroscopeApi::class.java)
-            val call = goroskopApi.getDailyHoroscope(zodiac = zodiacId)
-            call.enqueue(object : Callback<VedicastroapiResponse<HoroscopeResponse>> {
-
-                override fun onResponse(
-                    call: Call<VedicastroapiResponse<HoroscopeResponse>>,
-                    responseLocal: Response<VedicastroapiResponse<HoroscopeResponse>>
-                ) {
-
-                    val tr = responseLocal.body() as VedicastroapiResponse
-                    Timber.d(" \n api res = ${tr.response.bot_response.finances.split_response}  \n score = ${tr.response.bot_response.finances.score} ")
-
-                    root.horoscopeView.visibility = View.VISIBLE
-                    root.tvHoroscopePrediction.text = tr.response.bot_response.finances.split_response
-                    bBack.setOnClickListener {
-                        root.horoscopeView.visibility = View.GONE
-                        bBack.setOnClickListener(null)
-                    }
-                    if (zodiacId == 1) {
-                        tvOffer.visibility =View.VISIBLE
-                        tvOffer.setOnClickListener {
-                            //dfvefv
-                        }
-                    }
-                }
-
-                override fun onFailure(call: Call<VedicastroapiResponse<HoroscopeResponse>>, t: Throwable) {
-                    Timber.e(t.message ?: "")
-                }
-            })
-
-        }
-        recyclerView.adapter = photoAdapter
-
-        //add data
-        dataList.add(ZodiacModel(R.drawable.ic_virgo))
-        dataList.add(ZodiacModel(R.drawable.zodiac))
-        dataList.add(ZodiacModel(R.drawable.zodiac))
-
-        dataList.add(ZodiacModel(R.drawable.zodiac))
-        dataList.add(ZodiacModel(R.drawable.zodiac))
-        dataList.add(ZodiacModel(R.drawable.zodiac))
-
-        dataList.add(ZodiacModel(R.drawable.zodiac))
-        dataList.add(ZodiacModel(R.drawable.zodiac))
-        dataList.add(ZodiacModel(R.drawable.zodiac))
-
-        dataList.add(ZodiacModel(R.drawable.zodiac))
-        dataList.add(ZodiacModel(R.drawable.zodiac))
-        dataList.add(ZodiacModel(R.drawable.zodiac))
-
-        photoAdapter.setDataList(dataList)
     }
 }
--- app/src/main/java/com/paykey/brsju/kbutils/horoscope/HoroscopeView.kt
+++ app/src/main/java/com/paykey/brsju/kbutils/horoscope/HoroscopeView.kt
@@ -3,10 +3,18 @@ package com.paykey.brsju.kbutils.horoscope
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.paykey.brsju.kbutils.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import timber.log.Timber
 
 
 class HoroscopeView @JvmOverloads constructor(
@@ -17,6 +25,13 @@ class HoroscopeView @JvmOverloads constructor(
 
     lateinit var horoscopeView: LinearLayout
     lateinit var tvHoroscopePrediction: TextView
+    private lateinit var photoAdapter: PhotoAdapter
+    private var dataList = mutableListOf<ZodiacModel>()
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var bBack: ImageView
+    private lateinit var tvOffer: TextView
+    private lateinit var offerView: LinearLayout
+    private lateinit var ivOfferBack: ImageView
 
     init {
         init(attrs)
@@ -26,5 +41,71 @@ class HoroscopeView @JvmOverloads constructor(
         val view = inflate(context, R.layout.horoscope_layout, this)
         horoscopeView = view.findViewById(R.id.horoscopeView)
         tvHoroscopePrediction = view.findViewById(R.id.tvHoroscopePrediction)
+        recyclerView = view.findViewById(R.id.rvZodiacsGrid)
+        bBack = view.findViewById(R.id.ivBack)
+        tvOffer = view.findViewById(R.id.tvOffer)
+        offerView = view.findViewById(R.id.offerView)
+        ivOfferBack = view.findViewById(R.id.ivOfferBack)
+        recyclerView.layoutManager = GridLayoutManager(context, 3)
+
+        photoAdapter = PhotoAdapter {
+
+            val goroskopApi = HoroscopeApi.client().create(HoroscopeApi::class.java)
+            val call = goroskopApi.getDailyHoroscope(zodiac = zodiacId)
+            call.enqueue(object : Callback<VedicastroapiResponse<HoroscopeResponse>> {
+
+                override fun onResponse(
+                    call: Call<VedicastroapiResponse<HoroscopeResponse>>,
+                    responseLocal: Response<VedicastroapiResponse<HoroscopeResponse>>
+                ) {
+
+                    val tr = responseLocal.body() as VedicastroapiResponse
+                    Timber.d(" \n api res = ${tr.response.bot_response.finances.split_response}  \n score = ${tr.response.bot_response.finances.score} ")
+
+                    horoscopeView.visibility = View.VISIBLE
+                    tvHoroscopePrediction.text = tr.response.bot_response.finances.split_response
+                    bBack.setOnClickListener {
+                        horoscopeView.visibility = View.GONE
+                        bBack.setOnClickListener(null)
+                    }
+                    if (zodiacId == 1) {
+                        tvOffer.visibility = View.VISIBLE
+                        tvOffer.setOnClickListener {
+                            offerView.visibility = View.VISIBLE
+                            ivOfferBack.setOnClickListener {
+                                offerView.visibility = View.GONE
+                                ivOfferBack.setOnClickListener(null)
+                            }
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<VedicastroapiResponse<HoroscopeResponse>>, t: Throwable) {
+                    Timber.e(t.message ?: "")
+                }
+            })
+
+        }
+        recyclerView.adapter = photoAdapter
+
+        //add data
+        dataList.add(ZodiacModel(R.drawable.ic_virgo))
+        dataList.add(ZodiacModel(R.drawable.zodiac))
+        dataList.add(ZodiacModel(R.drawable.zodiac))
+
+        dataList.add(ZodiacModel(R.drawable.zodiac))
+        dataList.add(ZodiacModel(R.drawable.zodiac))
+        dataList.add(ZodiacModel(R.drawable.zodiac))
+
+        dataList.add(ZodiacModel(R.drawable.zodiac))
+        dataList.add(ZodiacModel(R.drawable.zodiac))
+        dataList.add(ZodiacModel(R.drawable.zodiac))
+
+        dataList.add(ZodiacModel(R.drawable.zodiac))
+        dataList.add(ZodiacModel(R.drawable.zodiac))
+        dataList.add(ZodiacModel(R.drawable.zodiac))
+
+        photoAdapter.setDataList(dataList)
+
     }
 }
